902954787512464211432da96436c6bf21af12f5
manjur0 <sfmanjur21@gmail.com>
2024-08-23 23:07:23 +0600

    movie details modal functionality added

--- src/Cine/MovieCard.jsx
+++ src/Cine/MovieCard.jsx
@@ -1,30 +1,53 @@
+import { useState } from "react";
 import { getImageUrl } from "../utils/cine-utility";
 import Rating from "./Rating";
+import MovieDetailsModal from "./MovieDetailsModal";
 const MovieCard = ({ movie }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  // modal close function
+  function handleCloseModla() {
+    setSelectedMovie(null);
+    setShowModal(false);
+  }
+
+  // movie selected show modal
+  function hadleMovieSelection(movie) {
+    setSelectedMovie(movie);
+    setShowModal(true);
+  }
   return (
-    <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-      <img
-        className="w-full object-cover"
-        src={getImageUrl(movie.cover)}
-        alt={movie.title}
-      />
-      <figcaption className="pt-4">
-        <h3 className="text-xl mb-1">{movie.title}</h3>
-        <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
+    <>
+      {showModal && (
+        <MovieDetailsModal movie={selectedMovie} onClose={handleCloseModla} />
+      )}
+      <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
+        <a href="#" onClick={() => hadleMovieSelection(movie)}>
+          <img
+            className="w-full object-cover"
+            src={getImageUrl(movie.cover)}
+            alt={movie.title}
+          />
+          <figcaption className="pt-4">
+            <h3 className="text-xl mb-1">{movie.title}</h3>
+            <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
 
-        <div className="flex items-center space-x-1 mb-5">
-          <Rating value={movie.rating} className="text-yellow-400" />
-        </div>
+            <div className="flex items-center space-x-1 mb-5">
+              <Rating value={movie.rating} className="text-yellow-400" />
+            </div>
 
-        <a
-          className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-          href="#"
-        >
-          <img src="./assets/tag.svg" alt="" />
-          <span> {movie.price} | Add to Cart</span>
+            <a
+              className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+              href="#"
+            >
+              <img src="./assets/tag.svg" alt="" />
+              <span> ${movie.price} | Add to Cart</span>
+            </a>
+          </figcaption>
         </a>
-      </figcaption>
-    </figure>
+      </figure>
+    </>
   );
 };
 
