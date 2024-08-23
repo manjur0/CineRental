8acb84ad4e042f7fe2aee660e4ec346823c8fecc
manjur0 <sfmanjur21@gmail.com>
2024-08-23 18:53:10 +0600

    Movie List and movie data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import MoviesList from "./Cine/MoviesList";
 import Headers from "./Components/Headers";
 import SideBar from "./Components/SideBar";
 
@@ -8,6 +9,7 @@ const App = () => {
       <main>
         <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
           <SideBar />
+          <MoviesList />
         </div>
       </main>
     </>
